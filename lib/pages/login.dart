cb3641cc2a592f108d7246c4f202f38f7dc41bd2
Martin Chaine <martin@casimir.me>
2023-07-26 23:31:14 +0200

    fix form display with native keyboard (and small screens)

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -21,7 +21,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
-      body: Column(
+      body: ListView(
         children: [
           ServerForm(
             stateKey: _serverFbKey,
