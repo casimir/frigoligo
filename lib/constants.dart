b420b6c62b58ec8a83721c15e5c497999d51b7eb
Martin Chaine <martin@casimir.me>
2023-08-07 18:20:50 +0200

    add optional unread app badge for iOS, macOS and some Android versions

--- lib/constants.dart
+++ lib/constants.dart
@@ -35,3 +35,6 @@ final Icon shareIcon = Platform.isIOS || Platform.isMacOS
 
 const int autoSyncThrottleSeconds = 15 * 60;
 const int logCountResetThreshold = 1000;
+
+final appBadgeSupported =
+    Platform.isIOS || Platform.isMacOS || Platform.isAndroid;
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import dynamic_color
+import flutter_app_badger
 import isar_flutter_libs
 import macos_ui
 import macos_window_utils
@@ -18,6 +19,7 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  FlutterAppBadgerPlugin.register(with: registry.registrar(forPlugin: "FlutterAppBadgerPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
   MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
