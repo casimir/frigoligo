a20e40287771f1406d68a500d2af4fa4dad41c1d
Martin Chaine <martin@casimir.me>
2024-03-19 21:05:29 +0100

    persist pending remote sync actions until there are sent to the server

--- lib/services/remote_sync_actions/articles.dart
+++ lib/services/remote_sync_actions/articles.dart
@@ -4,6 +4,12 @@ class RefreshArticlesAction extends RemoteSyncAction {
   const RefreshArticlesAction() : super('refreshArticles');
 
   @override
+  ActionParams params() => {};
+
+  factory RefreshArticlesAction.fromParams(ActionParams params) =>
+      const RefreshArticlesAction();
+
+  @override
   Future<void> execute(syncer) async {
     await syncer.wallabag!.incrementalRefresh(
       onProgress: (progress) => syncer.progressValue = progress,
@@ -15,6 +21,12 @@ class DeleteArticleAction extends RemoteSyncAction {
   const DeleteArticleAction(this.articleId) : super('deleteArticle:$articleId');
 
   final int articleId;
+
+  @override
+  ActionParams params() => {'articleId': articleId};
+
+  factory DeleteArticleAction.fromParams(ActionParams params) =>
+      DeleteArticleAction(params['articleId'] as int);
 
   @override
   Future<void> execute(syncer) async {
@@ -35,6 +47,22 @@ class EditArticleAction extends RemoteSyncAction {
   final bool? archive;
   final bool? starred;
   final List<String>? tags;
+
+  @override
+  ActionParams params() => {
+        'articleId': articleId,
+        'archive': archive,
+        'starred': starred,
+        'tags': tags,
+      };
+
+  factory EditArticleAction.fromParams(ActionParams params) =>
+      EditArticleAction(
+        params['articleId'] as int,
+        archive: params['archive'] as bool?,
+        starred: params['starred'] as bool?,
+        tags: (params['tags'] as List?)?.cast<String>(),
+      );
 
   @override
   Future<void> execute(syncer) async {
