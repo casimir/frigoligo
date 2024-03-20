c7d8e5cf3a632e4a6dcf064a272c5d5951520598
Martin Chaine <martin@casimir.me>
2024-03-20 14:15:35 +0100

    cleanup imports

--- lib/pages/session_details.dart
+++ lib/pages/session_details.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
 import '../buildcontext_extension.dart';
 import '../datetime_extension.dart';
