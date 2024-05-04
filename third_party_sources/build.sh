b53d05eec97ef6dd29a8ba236f07ef6798a21776
Martin Chaine <martin@casimir.me>
2024-05-04 22:07:51 +0200

    build third party binaries from sources

---
+++ third_party_sources/build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -e
+
+SCRIPT_PATH="$(readlink -f $0)"
+TPS_DIR="$(dirname $SCRIPT_PATH)"
+
+if [ ! -d "$PUB_CACHE" ]; then
+  echo "PUB_CACHE not set"
+  exit 1
+fi
+
+echo "> Building Isar native libs..."
+ISAR_VERSION=$(awk '/isar_version: /{print $3}' $TPS_DIR/../pubspec.yaml)
+ISAR_LIBS_DIR=$PUB_CACHE/hosted/isar-community.dev/isar_flutter_libs-$ISAR_VERSION/android/src/main/jniLibs
+echo "target directory: $ISAR_LIBS_DIR\n"
+pushd $TPS_DIR/isar
+./tool/build_android.sh x86
+mv libisar_android_x86.so $ISAR_LIBS_DIR/x86/libisar.so
+./tool/build_android.sh x64
+mv libisar_android_x64.so $ISAR_LIBS_DIR/x86_64/libisar.so
+./tool/build_android.sh armv7
+mv libisar_android_armv7.so $ISAR_LIBS_DIR/armeabi-v7a/libisar.so
+./tool/build_android.sh arm64
+mv libisar_android_arm64.so $ISAR_LIBS_DIR/arm64-v8a/libisar.so
+popd
+echo "> DONE"
+
+echo "> Building transistor-background-fetch artifact..."
+pushd $TPS_DIR/transistor-background-fetch/android/tsbackgroundfetch/
+gradle assembleRelease
+gradle publishToMavenLocal
+popd
+echo "> DONE"
