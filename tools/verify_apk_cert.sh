36692e737b3222dabb5f62a536efc4ea60d94c36
Martin Chaine <martin@casimir.me>
2024-05-14 18:10:18 +0200

    Release ABI specific APKs (#187)
    * validate apk signature before release upload
    
    * build the APKs in a new step (+ prepare signature)
    
    * use a more compatible syntax
    
    * autodetect apksigner path
    
    * add signature check and upload
    
    * fix double print
    
    * fix release and upload
    
    * rework signingConfig logic
    
    * prevent obfuscation of "frigoligo" in logs
    
    * make multiline run more lisible
    
    * fix keystore path
    
    * fix keystore file name
    
    * fix initial path
    
    * just make one archive...
    
    * upgrade to upload v4
    
    * upgrade artifact action everywhere
    
    * use builtin signing in all android steps

---
+++ tools/verify_apk_cert.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+VALID_HASH=5ac383c981bb53ea5a873cf6086d0a6a0a2f557420694ed5ae4c495677aafd5c
+
+apksigner_bin=$APKSIGNER_BIN
+if [ -z "$apksigner_bin" ]; then
+    flutter_android_home=$(flutter config --list | awk '/android-sdk: (.+)/{print $2}')
+    android_home=${ANDROID_HOME:-$flutter_android_home}
+    apksigner_bin=$(ls $android_home/build-tools/*/apksigner | head -n1)
+fi
+apksigner_bin=${apksigner_bin:-apksigner}
+
+$apksigner_bin >/dev/null || exit 1
+>&2 echo "apksigner: $apksigner_bin"
+
+for f in "$@"; do
+    fingerprint=$($apksigner_bin verify --print-certs $f 2>/dev/null | sed -n 's/^Signer #1 certificate SHA-256 digest: \(.*\)/\1/p')
+    [ "$fingerprint" = "$VALID_HASH" ] || (echo "$f: bad certificate: $fingerprint" && exit 1) && echo "$f: OK"
+done
