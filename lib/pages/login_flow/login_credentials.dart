d11147105c116ce5d92962b692a16dddb284b8ff
Martin Chaine <martin@casimir.me>
2024-07-19 13:59:07 +0200

    factorize login flow code

--- lib/pages/login_flow/login_credentials.dart
+++ lib/pages/login_flow/login_credentials.dart
@@ -1,39 +1,198 @@
-import 'package:flutter/widgets.dart';
+import 'package:adaptive_dialog/adaptive_dialog.dart';
+import 'package:cadanse/cadanse.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:logging/logging.dart';
 
+import '../../buildcontext_extension.dart';
+import '../../providers/server_login_flow.dart';
 import '../../server/check.dart';
+import '../../server/providers/client.dart';
 import '../../server/session.dart';
+import '../../wallabag/client.dart';
+import '../../widget_keys.dart';
 import 'login_freon.dart';
 import 'login_wallabag.dart';
+import 'utils.dart';
 
-abstract class LoginFlowCredentials extends Widget {
-  factory LoginFlowCredentials({
-    Key? key,
-    required ServerCheck serverCheck,
-    Map<String, String> initial = const {},
-    void Function()? onReset,
+final _log = Logger('login');
+
+class LoginFlowCredentials extends ConsumerStatefulWidget {
+  LoginFlowCredentials({
+    super.key,
+    required this.serverCheck,
+    this.initial = const {},
+    this.onReset,
   }) {
     final rawServerType = serverCheck.info!.appname;
     final serverType = ServerType.values.byName(rawServerType);
 
     switch (serverType) {
       case ServerType.freon:
-        return LoginFlowFreon(
-          key: key,
-          serverCheck: serverCheck,
-          initial: initial,
-          onReset: onReset,
-        );
+        loginController = FreonLoginFlowController();
       case ServerType.wallabag:
-        return LoginFlowWallabag(
-          key: key,
-          serverCheck: serverCheck,
-          initial: initial,
-          onReset: onReset,
-        );
+        loginController = WallabagLoginFlowController();
       case ServerType.unknown:
         throw UnknownServerTypeError(rawServerType);
     }
   }
+
+  final ServerCheck serverCheck;
+  late final LoginFlowController loginController;
+  final Map<String, String> initial;
+  final void Function()? onReset;
+
+  @override
+  ConsumerState<LoginFlowCredentials> createState() =>
+      _LoginFlowCredentialsState();
+}
+
+class _LoginFlowCredentialsState extends ConsumerState<LoginFlowCredentials> {
+  final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
+  bool _gotAnError = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.loginController.dataIsExhaustive(widget.initial)) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        attemptLogin();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final loginFields = widget.loginController
+        .getFields(context)
+        .map(
+          (e) => FormBuilderTextField(
+            key: e.key,
+            name: e.name,
+            validator: (value) => notEmptyValidator(context, value, e.label),
+            decoration: InputDecoration(
+              icon: e.icon,
+              labelText: e.label,
+            ),
+            autofocus: true,
+            autocorrect: false,
+          ) as Widget,
+        )
+        .toList();
+
+    return Center(
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          Hero(
+            tag: 'loginFlowServer',
+            child: ListTile(
+              leading: const Icon(Icons.home),
+              title: Text(
+                widget.serverCheck.uri!.toString(),
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(
+                  '${widget.serverCheck.info!.appname} ${widget.serverCheck.info!.version}'),
+              trailing: IconButton(
+                icon: const Icon(Icons.edit),
+                onPressed: () {
+                  ref.read(serverLoginFlowProvider.notifier).reset();
+                  widget.onReset?.call();
+                },
+              ),
+            ),
+          ),
+          C.spacers.verticalContent,
+          Padding(
+            padding: C.paddings.defaultPadding,
+            child: FormBuilder(
+              key: _formKey,
+              initialValue: widget.initial,
+              child: AutofillGroup(
+                // It would be nice to provide a domain for autofill but it's dynamic.
+                // https://developer.apple.com/documentation/xcode/supporting-associated-domains
+                child: Column(
+                  children: loginFields +
+                      [
+                        C.spacers.verticalContent,
+                        ElevatedButton(
+                          key: const Key(wkLoginFlowLogIn),
+                          onPressed: () => attemptLogin(),
+                          child: Text(context.L.login_actionLogin),
+                        ),
+                        C.spacers.verticalContent,
+                        if (_gotAnError)
+                          MaterialButton(
+                            onPressed: () => context.push('/logs'),
+                            child: Text(context.L.login_openLogConsole),
+                          ),
+                      ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> attemptLogin() async {
+    if (_formKey.currentState!.saveAndValidate()) {
+      try {
+        final session = await widget.loginController
+            .openSession(widget.serverCheck, _formKey.currentState!.value);
+        await session.save();
+        ref.invalidate(sessionProvider);
+        if (mounted) {
+          context.go('/');
+        }
+      } catch (e) {
+        setState(() => _gotAnError = true);
+        if (e is ServerError) {
+          _log.warning('authentication failed', e.message);
+          if (mounted) {
+            showOkAlertDialog(context: context, message: e.message);
+          }
+        } else {
+          _log.severe('unexpected error', e);
+          if (mounted) {
+            showOkAlertDialog(context: context, message: e.toString());
+          }
+        }
+      }
+    }
+  }
+}
+
+abstract class LoginFlowController {
+  bool dataIsExhaustive(Map<String, String> data);
+  List<LoginField> getFields(BuildContext context);
+  Future<ServerSession> openSession(
+      ServerCheck check, Map<String, dynamic> values);
+}
+
+class LoginField {
+  const LoginField({
+    required this.name,
+    required this.label,
+    required this.icon,
+    this.key,
+    this.obscureText = false,
+    this.autofocus = false,
+    this.autofillHints,
+  });
+
+  final String name;
+  final String label;
+  final Icon icon;
+  final Key? key;
+  final bool obscureText;
+  final bool autofocus;
+  final List<String>? autofillHints;
 }
 
 class UnknownServerTypeError implements Exception {
--- lib/pages/login_flow/login_freon.dart
+++ lib/pages/login_flow/login_freon.dart
@@ -1,155 +1,36 @@
-import 'package:adaptive_dialog/adaptive_dialog.dart';
-import 'package:cadanse/cadanse.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../buildcontext_extension.dart';
-import '../../providers/server_login_flow.dart';
 import '../../server/check.dart';
 import '../../server/freon.dart';
-import '../../server/providers/client.dart';
 import '../../server/session.dart';
 import '../../wallabag/client.dart';
-import '../../widget_keys.dart';
 import 'login_credentials.dart';
-import 'utils.dart';
 
-class LoginFlowFreon extends ConsumerStatefulWidget
-    implements LoginFlowCredentials {
-  const LoginFlowFreon({
-    super.key,
-    required this.serverCheck,
-    this.initial = const {},
-    this.onReset,
-  });
-
-  final ServerCheck serverCheck;
-  final Map<String, String> initial;
-  final void Function()? onReset;
+class FreonLoginFlowController implements LoginFlowController {
+  @override
+  bool dataIsExhaustive(Map<String, String> data) =>
+      data.isNotEmpty && data.containsKey('apiToken');
 
   @override
-  ConsumerState<LoginFlowFreon> createState() => _LoginFlowFreonState();
-}
-
-class _LoginFlowFreonState extends ConsumerState<LoginFlowFreon> {
-  final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
-  bool _gotAnError = false;
-
-  @override
-  void initState() {
-    super.initState();
-
-    final initialIsExhaustive =
-        widget.initial.isNotEmpty && widget.initial.containsKey('apiToken');
-    if (initialIsExhaustive) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        attemptLogin(context);
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ListView(
-        shrinkWrap: true,
-        children: [
-          Hero(
-            tag: 'loginFlowServer',
-            child: ListTile(
-              leading: const Icon(Icons.home),
-              title: Text(
-                widget.serverCheck.uri!.toString(),
-                style: const TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text(
-                  '${widget.serverCheck.info!.appname} ${widget.serverCheck.info!.version}'),
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () {
-                  ref.read(serverLoginFlowProvider.notifier).reset();
-                  widget.onReset?.call();
-                },
-              ),
-            ),
-          ),
-          C.spacers.verticalContent,
-          Padding(
-            padding: C.paddings.defaultPadding,
-            child: FormBuilder(
-              key: _formKey,
-              initialValue: widget.initial,
-              child: AutofillGroup(
-                // It would be nice to provide a domain for autofill but it's dynamic.
-                // https://developer.apple.com/documentation/xcode/supporting-associated-domains
-                child: Column(
-                  children: [
-                    FormBuilderTextField(
-                      name: 'apiToken',
-                      validator: (value) => notEmptyValidator(
-                          context, value, context.L.session_fieldApiToken),
-                      decoration: InputDecoration(
-                        icon: const Icon(Icons.token),
-                        labelText: context.L.session_fieldApiToken,
-                      ),
-                      autofocus: true,
-                      autocorrect: false,
-                    ),
-                    C.spacers.verticalContent,
-                    ElevatedButton(
-                      key: const Key(wkLoginFlowLogIn),
-                      onPressed: () => attemptLogin(context),
-                      child: Text(context.L.login_actionLogin),
-                    ),
-                    C.spacers.verticalContent,
-                    if (_gotAnError)
-                      MaterialButton(
-                        onPressed: () => context.push('/logs'),
-                        child: Text(context.L.login_openLogConsole),
-                      ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Future<void> attemptLogin(BuildContext context) async {
-    if (_formKey.currentState!.saveAndValidate()) {
-      final session = ServerSession(
-        ServerType.freon,
-        freon: FreonCredentials(
-          widget.serverCheck.uri!,
-          _formKey.currentState!.value['apiToken'],
+  List<LoginField> getFields(BuildContext context) => [
+        LoginField(
+          name: 'apiToken',
+          label: context.L.session_fieldApiToken,
+          icon: const Icon(Icons.token),
+          autofocus: true,
         ),
-      );
-      try {
-        final freon = FreonClient(session.freon!);
-        await freon.wallabag.getInfo();
-        await session.save();
-        ref.invalidate(sessionProvider);
-        if (context.mounted) {
-          context.go('/');
-        }
-      } catch (e) {
-        setState(() => _gotAnError = true);
-        if (e is ServerError) {
-          logger.warning('authentication failed', e.message);
-          if (context.mounted) {
-            showOkAlertDialog(context: context, message: e.message);
-          }
-        } else {
-          logger.severe('unexpected error', e);
-          if (context.mounted) {
-            showOkAlertDialog(context: context, message: e.toString());
-          }
-        }
-      }
-    }
+      ];
+
+  @override
+  Future<ServerSession> openSession(
+      ServerCheck check, Map<String, dynamic> values) async {
+    final session = ServerSession(
+      ServerType.freon,
+      freon: FreonCredentials(check.uri!, values['apiToken']),
+    );
+    final freon = FreonClient(session.freon!);
+    await freon.wallabag.getInfo();
+    return session;
   }
 }
