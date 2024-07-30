fffea7d7d3942d2c404071b93c25f517e48b3939
Martin Chaine <martin@casimir.me>
2024-07-30 19:47:18 +0200

    fix a typo in log format

--- lib/utils.dart
+++ lib/utils.dart
@@ -2,7 +2,7 @@ import 'package:logging/logging.dart';
 
 String loglineFromRecord(LogRecord record) {
   var line =
-      '[$record.time] ${record.level.name} ${record.loggerName} ${record.message}';
+      '[${record.time}] ${record.level.name} ${record.loggerName} ${record.message}';
   if (record.error != null) {
     line += ' (${record.error})';
   }
