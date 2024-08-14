d9506988e8d5cab6c96931ad7f4c82d63ab36b79
casimir <casimir@users.noreply.github.com>
2024-08-14 17:25:38 +0000

    deploy: 0184cf5dfb64a5754d0b1429ac79c5b15ffdfa53

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -85,7 +85,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "29e20395122d9ba2a6a9dd4bd2a0a03e",
 "sqlite3.wasm": "9d4d8aa7522ca11c72eb148d1f23a3df",
-"flutter_bootstrap.js": "8f337d192617c1cac057ebc89a8f7c30",
+"flutter_bootstrap.js": "db39eddbe4d5d5126de25c0705b1af72",
 "drift_worker.js": "b1b42d2639a16f676982db426871cdba",
 "canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
 "canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
