ab8e88fa8606e635fce1cdde77ed9b5789be7746
Martin Chaine <martin@casimir.me>
2024-08-14 17:01:18 +0200

    AppBagePlugin, implement macOS native code

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -3,13 +3,14 @@ import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
-    let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
+      let flutterViewController = FlutterViewController()
+      let windowFrame = self.frame
+      self.contentViewController = flutterViewController
+      self.setFrame(windowFrame, display: true)
 
-    RegisterGeneratedPlugins(registry: flutterViewController)
+      RegisterGeneratedPlugins(registry: flutterViewController)
+      AppBadgePlugin.register(with: flutterViewController.registrar(forPlugin: "AppBadgePlugin"))
 
-    super.awakeFromNib()
+      super.awakeFromNib()
   }
 }
