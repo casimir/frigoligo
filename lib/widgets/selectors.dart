c857395a7da458f8012a44bf5ccfac491a8f868c
Martin Chaine <martin@casimir.me>
2024-09-15 12:35:43 +0200

    introduce a modal bottom sheet specialized in multi values selection

---
+++ lib/widgets/selectors.dart
@@ -0,0 +1,181 @@
+import 'package:cadanse/cadanse.dart';
+import 'package:cadanse/components/widgets/error.dart';
+import 'package:flutter/material.dart';
+
+import '../buildcontext_extension.dart';
+import '../pages/listing.dart';
+import '../pages/listing/search.dart';
+
+class MultiSelect<T> extends StatefulWidget {
+  const MultiSelect({
+    super.key,
+    required this.title,
+    required this.entries,
+    this.onConfirm,
+  });
+
+  final String title;
+  final List<DropdownMenuEntry<T>> entries;
+  final void Function(Iterable<T>)? onConfirm;
+
+  @override
+  State<MultiSelect<T>> createState() => _MultiSelectState<T>();
+}
+
+class _MultiSelectState<T> extends State<MultiSelect<T>> {
+  late final Map<String, DropdownMenuEntry<T>> _index;
+  final TextEditingController _searchController = TextEditingController();
+  final Set<T> _selected = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _index = {for (final it in widget.entries) it.label: it};
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      Dialog.fullscreen(child: _buildChild(context));
+
+  Widget _buildChild(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: Text(widget.title),
+        actions: [
+          TextButton(
+            child: Text(MaterialLocalizations.of(context).okButtonLabel),
+            onPressed: () {
+              widget.onConfirm?.call(_selected);
+              Navigator.of(context).pop(_selected);
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: C.paddings.defaultPadding,
+        child: Column(
+          children: [
+            Row(children: [
+              Expanded(
+                child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    icon: const Icon(Icons.search),
+                    hintText: context.L.g_search,
+                  ),
+                  onChanged: (_) => setState(() {}),
+                  autocorrect: false,
+                ),
+              ),
+              spaceHorizontalInGroup,
+              IconButton(
+                  icon: const Icon(Icons.refresh),
+                  onPressed: () {
+                    _selected.clear();
+                    _searchController.clear();
+                    setState(() {});
+                  })
+            ]),
+            spacerVerticalInGroup,
+            Expanded(
+              child: ListView(
+                  children: search(_searchController.text, _index.keys.toList())
+                      .map((m) => _buildItem(context, _index[m.entry]!))
+                      .toList()),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildItem(BuildContext context, DropdownMenuEntry<T> entry) {
+    final isSelected = _selected.contains(entry.value);
+    return ListTile(
+      title: Text(entry.label),
+      leading: entry.leadingIcon,
+      onTap: () {
+        if (_selected.contains(entry.value)) {
+          _selected.remove(entry.value);
+        } else {
+          _selected.add(entry.value);
+        }
+        setState(() {});
+      },
+      trailing: isSelected ? const Icon(Icons.check) : const SizedBox(),
+      iconColor: isSelected ? Theme.of(context).colorScheme.primary : null,
+      textColor: isSelected ? Theme.of(context).colorScheme.primary : null,
+    );
+  }
+}
+
+class EntryScore {
+  EntryScore(this.entry, this.score);
+
+  final String entry;
+  final double score;
+
+  @override
+  String toString() => '$entry ($score)';
+}
+
+List<EntryScore> search(String query, List<String> entries) {
+  if (query.isEmpty) {
+    return entries.map((e) => EntryScore(e, 0.0)).toList()
+      ..sort((a, b) => a.entry.compareTo(b.entry));
+  }
+
+  final preparedQuery =
+      query.split(RegExp(r'\s+')).map((e) => '($e)').join('.*');
+  final re = RegExp(preparedQuery, caseSensitive: false);
+  return entries
+      .map((e) => EntryScore(e, _computeScore(re.allMatches(e))))
+      .where((e) => e.score > 0.0)
+      .toList()
+    ..sort((a, b) {
+      final delta = b.score.compareTo(a.score);
+      return delta == 0 ? a.entry.compareTo(b.entry) : delta;
+    });
+}
+
+double _computeScore(Iterable<RegExpMatch> matches) {
+  if (matches.isEmpty) return 0;
+
+  final start = matches.first.start;
+  final end = matches.last.end;
+  return 100 / (1 + end - start);
+}
+
+Future<Iterable<T>?> showBottomSheetSelector<T>({
+  required BuildContext context,
+  required String title,
+  required Future<Iterable<T>> entriesBuilder,
+}) {
+  return showModalBottomSheet(
+    context: context,
+    builder: (_) => FutureBuilder(
+        future: entriesBuilder,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator.adaptive());
+          }
+          if (snapshot.hasError) {
+            // TODO center vertically
+            return Center(child: ErrorScreen(error: snapshot.error!));
+          }
+
+          return MultiSelect(
+            title: title,
+            entries: snapshot.data!
+                .map((it) => DropdownMenuEntry(value: it, label: it.toString()))
+                .toList(),
+          );
+        }),
+    isScrollControlled: true,
+    useSafeArea: true,
+  );
+}
