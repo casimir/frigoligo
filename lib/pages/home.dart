59c736bfc23605d2636ec1e9bde1140830746307
Martin Chaine <martin@casimir.me>
2024-10-26 15:56:56 +0200

    bump cadanse

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -39,8 +39,8 @@ class _HomePageState extends ConsumerState<HomePage> {
   Widget build(BuildContext context) {
     return switch (Layout.windowClass(context)) {
       WindowClass.compact => _buildNarrowLayout(),
-      WindowClass.medium => _buildWideLayout(),
-      WindowClass.expanded => _buildDynamicLayout(),
+      WindowClass.medium => _buildDynamicLayout(),
+      WindowClass.expanded => _buildWideLayout(),
     };
   }
 
