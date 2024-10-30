02e44d01edb00b90171909fd876545e15dd50505
Martin Chaine <martin@casimir.me>
2024-10-30 16:00:22 +0100

    rework the reading screen build logic

--- lib/pages/reading/article.dart
+++ lib/pages/reading/article.dart
@@ -1,28 +1,22 @@
 import 'package:adaptive_dialog/adaptive_dialog.dart';
-import 'package:cadanse/components/widgets/error.dart';
 import 'package:cadanse/layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
-import 'package:fwfh_cached_network_image/fwfh_cached_network_image.dart';
-import 'package:fwfh_url_launcher/fwfh_url_launcher.dart';
 import 'package:go_router/go_router.dart';
 import 'package:share_plus/share_plus.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import '../../buildcontext_extension.dart';
 import '../../constants.dart';
 import '../../db/extensions/article.dart';
 import '../../db/models/article.drift.dart';
-import '../../providers/article.dart';
 import '../../providers/expander.dart';
-import '../../providers/reading_settings.dart';
 import '../../services/remote_sync.dart';
 import '../../services/remote_sync_actions/articles.dart';
 import '../../widget_keys.dart';
 import '../../widgets/remote_sync_fab.dart';
 import '../../widgets/remote_sync_progress_indicator.dart';
 import '../reading_settings_configurator.dart';
+import 'article_content.dart';
 import 'article_sheet.dart';
 import 'mixins.dart';
 
@@ -31,14 +25,12 @@ class ArticlePage extends ConsumerStatefulWidget {
     super.key,
     this.drawer,
     this.forcedDrawerOpen = false,
-    this.withBottomBar,
     this.withExpander = false,
     this.withProgressIndicator = true,
   });
 
   final Widget? drawer;
   final bool forcedDrawerOpen;
-  final bool? withBottomBar;
   final bool withExpander;
   final bool withProgressIndicator;
 
@@ -63,192 +55,78 @@ class _ArticlePageState extends ConsumerState<ArticlePage>
   }
 
   @override
+  void dispose() {
+    scroller.dispose();
+
+    super.dispose();
+  }
+
+  Widget? get appBarLeading {
+    if (!widget.withExpander) return null;
+
+    return IconButton(
+      key: const Key(wkArticleExpanderToggle),
+      icon: Icon(
+        ref.watch(expanderProvider)
+            ? Icons.list
+            : Icons.keyboard_double_arrow_left,
+      ),
+      onPressed: () => ref.watch(expanderProvider.notifier).toggle(),
+    );
+  }
+
+  @override
   Widget buildArticle(BuildContext context, Article? article) {
-    Widget? leading;
-    if (widget.withExpander) {
-      leading = IconButton(
-        key: const Key(wkArticleExpanderToggle),
-        icon: Icon(
-          ref.watch(expanderProvider)
-              ? Icons.list
-              : Icons.keyboard_double_arrow_left,
-        ),
-        onPressed: () => ref.watch(expanderProvider.notifier).toggle(),
-      );
-    }
-
-    late final Widget body;
-    if (article == null) {
-      body = _buildNoArticle();
-    } else if (article.content == null) {
-      body = _buildEmptyContent(Uri.parse(article.url));
-    } else {
-      body = _buildArticleContent(article);
-    }
-
-    final showBottomBar = (widget.withBottomBar ?? true) &&
-        !Layout.isExpanded(context) &&
-        article != null;
+    final showBottomBar = !Layout.isExpanded(context);
     final showRemoteSyncerWidgets = widget.withProgressIndicator &&
         !(_scaffoldKey.currentState?.isDrawerOpen ?? false);
 
-    return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        leading: leading,
-        title: article != null
-            ? Builder(builder: (context) {
-                return InkWell(
-                  child: Text(article.title),
-                  onTap: () => Scaffold.of(context).openEndDrawer(),
-                );
-              })
-            : null,
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        actions: [
-          Builder(builder: (context) {
-            return IconButton(
-              icon: const Icon(Icons.info_outline),
-              onPressed: () => Scaffold.of(context).openEndDrawer(),
-            );
-          }),
-          if (!showBottomBar) ..._buildActions(article),
-          IconButton(
-            key: const Key(wkArticlePopupMenuSettings),
-            icon: const Icon(Icons.format_size),
-            onPressed: () => showModalBottomSheet(
-              context: context,
-              builder: (_) => const ReadingSettingsConfigurator(),
-              barrierColor: Colors.transparent,
-              showDragHandle: true,
-            ),
+    if (article == null) {
+      return _PageScaffold(
+        scaffoldKey: _scaffoldKey,
+        appBarLeading: appBarLeading,
+        title: null,
+        drawer: widget.drawer,
+        forcedDrawerOpen: widget.forcedDrawerOpen,
+        withProgressIndicator: showRemoteSyncerWidgets,
+        scrollEnabled: false,
+        builder: (_, __) => const Center(child: Icon(Icons.question_mark)),
+      );
+    }
+
+    return _PageScaffold(
+      scaffoldKey: _scaffoldKey,
+      controller: scroller,
+      appBarLeading: appBarLeading,
+      title: Text(article.title), // TODO make it tapable (GestureDetector)
+      actions: [
+        Builder(builder: (context) {
+          return IconButton(
+            icon: const Icon(Icons.info_outline),
+            onPressed: () => Scaffold.of(context).openEndDrawer(),
+          );
+        }),
+        if (!showBottomBar) ..._buildActions(article),
+        IconButton(
+          key: const Key(wkArticlePopupMenuSettings),
+          icon: const Icon(Icons.format_size),
+          onPressed: () => showModalBottomSheet(
+            context: context,
+            builder: (_) => const ReadingSettingsConfigurator(),
+            barrierColor: Colors.transparent,
+            showDragHandle: true,
           ),
-        ],
-      ),
-      body: SelectionArea(
-        child: Column(
-          children: [
-            if (showRemoteSyncerWidgets && !showBottomBar)
-              const RemoteSyncProgressIndicator(),
-            Expanded(child: body),
-            if (showRemoteSyncerWidgets && showBottomBar)
-              const RemoteSyncProgressIndicator(),
-          ],
         ),
-      ),
-      bottomNavigationBar: showBottomBar ? _buildBottomBar(article!) : null,
-      floatingActionButton: RemoteSyncFAB(showIf: showRemoteSyncerWidgets),
-      floatingActionButtonLocation:
-          showBottomBar ? FloatingActionButtonLocation.endContained : null,
+      ],
+      bottomActions: showBottomBar ? _buildActions(article) : null,
       drawer: widget.drawer,
-      onDrawerChanged: (isOpened) => setState(() {
-        // set the state so that the progress indicator widget move correctly
-      }),
-      endDrawer: article != null ? const ArticleSheet() : null,
-    );
-  }
-
-  Widget _buildNoArticle() {
-    return const Center(child: Icon(Icons.question_mark));
-  }
-
-  Widget _buildEmptyContent(Uri articleUrl) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            context.L.article_noContentFetched,
-            style: Theme.of(context).textTheme.headlineSmall,
-          ),
-          const SizedBox(height: 8.0),
-          ElevatedButton(
-            onPressed: () => launchUrl(articleUrl),
-            child: Text(context.L.article_openInBrowser),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildArticleContent(Article article) {
-    final scrollPositionFetch = ref.watch(scrollPositionProvider(article.id));
-    return scrollPositionFetch.when(
-      data: (scrollPosition) {
-        return NotificationListener<ScrollNotification>(
-          onNotification: (notification) {
-            if (notification is ScrollEndNotification) {
-              final progress =
-                  scroller.offset / scroller.position.maxScrollExtent;
-              ref
-                  .read(currentArticleProvider.notifier)
-                  .saveScrollProgress(progress);
-            }
-            return false;
-          },
-          child: Scrollbar(
-            controller: scroller,
-            child: SingleChildScrollView(
-              controller: scroller,
-              child: Column(
-                children: [
-                  _buildHeader(article),
-                  const Divider(),
-                  _buildContent(article.content!, scrollPosition),
-                  SizedBox(height: MediaQuery.paddingOf(context).bottom),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-      error: (e, _) => Center(child: ErrorScreen(error: e)),
-      loading: () => const Center(child: CircularProgressIndicator.adaptive()),
-    );
-  }
-
-  Widget _buildHeader(Article article) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Text(
-            article.title,
-            style: Theme.of(context).textTheme.headlineSmall,
-          ),
-          const SizedBox(height: 8.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(article.domainName ?? article.url),
-              const Text(' - '),
-              const Icon(Icons.timer_outlined),
-              Text(context.L.article_readingTime(article.readingTime)),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildContent(String content, ArticleScrollPosition? scrollPosition) {
-    final values = ref.watch(readingSettingsProvider);
-
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: HtmlWidget(
-        content,
-        factoryBuilder: () => HtmlWidgetFactory(
-          onTreeBuilt: (_) => WidgetsBinding.instance.addPostFrameCallback((_) {
-            if (scrollPosition != null && scrollPosition.progress > 0) {
-              final pixels =
-                  scrollPosition.progress * scroller.position.maxScrollExtent;
-              scroller.jumpTo(pixels);
-            }
-          }),
-        ),
-        textStyle: values.textStyle,
-      ),
+      forcedDrawerOpen: widget.forcedDrawerOpen,
+      endDrawer: const ArticleSheet(),
+      withProgressIndicator: showRemoteSyncerWidgets,
+      scrollEnabled: article.content != null,
+      builder: (_, scrollKey) => article.content == null
+          ? ArticleContentEmpty(articleUrl: Uri.parse(article.url))
+          : ArticleContent(article: article, scrollKey: scrollKey!),
     );
   }
 
@@ -310,24 +188,161 @@ class _ArticlePageState extends ConsumerState<ArticlePage>
       ),
     ];
   }
+}
 
-  Widget _buildBottomBar(Article article) {
-    return BottomAppBar(
-      child: Row(children: _buildActions(article)),
+enum _ProgressIndicatorPosition { none, top, bottom }
+
+typedef _PageScaffoldBodyBuilder = Widget Function(
+    BuildContext, GlobalKey<NestedScrollViewState>?);
+
+class _PageScaffold extends StatefulWidget {
+  const _PageScaffold({
+    required this.scaffoldKey,
+    this.controller,
+    required this.appBarLeading,
+    this.title,
+    this.actions = const [],
+    this.bottomActions,
+    this.drawer,
+    required this.forcedDrawerOpen,
+    this.endDrawer,
+    required this.withProgressIndicator,
+    this.scrollEnabled = true,
+    required this.builder,
+  });
+
+  final GlobalKey<ScaffoldState> scaffoldKey;
+  final ScrollController? controller;
+  final Widget? appBarLeading;
+  final Widget? title;
+  final List<Widget> actions;
+  final List<Widget>? bottomActions;
+  final Widget? drawer;
+  final bool forcedDrawerOpen;
+  final Widget? endDrawer;
+  final bool withProgressIndicator;
+  final bool scrollEnabled;
+  final _PageScaffoldBodyBuilder builder;
+
+  @override
+  State<_PageScaffold> createState() => _PageScaffoldState();
+}
+
+class _PageScaffoldState extends State<_PageScaffold> {
+  final GlobalKey<NestedScrollViewState> _scrollKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    var showProgressIndicator = _ProgressIndicatorPosition.none;
+    if (widget.withProgressIndicator) {
+      showProgressIndicator = widget.bottomActions != null
+          ? _ProgressIndicatorPosition.bottom
+          : _ProgressIndicatorPosition.top;
+    }
+    final appBar = SliverAppBar.large(
+      leading: widget.appBarLeading,
+      title: widget.title,
+      actions: widget.actions,
+      backgroundColor: WidgetStateColor.resolveWith((Set<WidgetState> states) {
+        final scheme = Theme.of(context).colorScheme;
+        return states.contains(WidgetState.scrolledUnder)
+            ? scheme.primaryContainer
+            : scheme.surface;
+      }),
+    );
+    final body = widget.scrollEnabled
+        ? _ScrollableContent(
+            scrollKey: _scrollKey,
+            appBar: appBar,
+            showProgressIndicator: showProgressIndicator,
+            body: widget.builder(context, _scrollKey),
+          )
+        : _NonScrollableContent(
+            appBar: appBar,
+            showProgressIndicator: showProgressIndicator,
+            body: widget.builder(context, null),
+          );
+
+    final showRemoteSyncerWidgets = widget.withProgressIndicator &&
+        !(widget.scaffoldKey.currentState?.isDrawerOpen ?? false);
+
+    return Scaffold(
+      key: widget.scaffoldKey,
+      body: body,
+      floatingActionButton: RemoteSyncFAB(showIf: showRemoteSyncerWidgets),
+      floatingActionButtonLocation: widget.bottomActions != null
+          ? FloatingActionButtonLocation.endDocked
+          : null,
+      drawer: widget.drawer,
+      onDrawerChanged: (isOpened) => setState(() {
+        // set the state so that the progress indicator widget move correctly
+      }),
+      endDrawer: widget.endDrawer,
+      bottomNavigationBar: widget.bottomActions != null
+          ? BottomAppBar(child: Row(children: widget.bottomActions!))
+          : null,
     );
   }
 }
 
-class HtmlWidgetFactory extends WidgetFactory
-    with CachedNetworkImageFactory, UrlLauncherFactory {
-  HtmlWidgetFactory({this.onTreeBuilt});
+class _NonScrollableContent extends StatelessWidget {
+  const _NonScrollableContent({
+    required this.appBar,
+    required this.showProgressIndicator,
+    required this.body,
+  });
 
-  void Function(Widget)? onTreeBuilt;
+  final SliverAppBar appBar;
+  final _ProgressIndicatorPosition showProgressIndicator;
+  final Widget body;
 
   @override
-  Widget buildBodyWidget(BuildContext context, Widget child) {
-    final body = super.buildBodyWidget(context, child);
-    onTreeBuilt?.call(body);
-    return body;
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      physics: const NeverScrollableScrollPhysics(),
+      slivers: [
+        appBar,
+        if (showProgressIndicator == _ProgressIndicatorPosition.top)
+          const SliverToBoxAdapter(child: RemoteSyncProgressIndicator()),
+        SliverFillRemaining(child: body),
+        if (showProgressIndicator == _ProgressIndicatorPosition.bottom)
+          const SliverToBoxAdapter(
+            child: RemoteSyncProgressIndicator(idleWidget: SizedBox.shrink()),
+          ),
+      ],
+    );
+  }
+}
+
+class _ScrollableContent extends StatelessWidget {
+  const _ScrollableContent({
+    this.scrollKey,
+    required this.appBar,
+    required this.showProgressIndicator,
+    required this.body,
+  });
+
+  final GlobalKey<NestedScrollViewState>? scrollKey;
+  final SliverAppBar appBar;
+  final _ProgressIndicatorPosition showProgressIndicator;
+  final Widget body;
+
+  @override
+  Widget build(BuildContext context) {
+    return NestedScrollView(
+      key: scrollKey,
+      headerSliverBuilder: (context, _) => [
+        appBar,
+        if (showProgressIndicator == _ProgressIndicatorPosition.top)
+          const RemoteSyncProgressIndicator(idleWidget: SizedBox.shrink()),
+      ],
+      body: Column(
+        children: [
+          Expanded(child: body),
+          if (showProgressIndicator == _ProgressIndicatorPosition.bottom)
+            const RemoteSyncProgressIndicator(idleWidget: SizedBox.shrink()),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/pages/reading/article_content.dart
@@ -0,0 +1,160 @@
+import 'package:cadanse/cadanse.dart';
+import 'package:cadanse/components/layouts/grouping.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
+import 'package:fwfh_cached_network_image/fwfh_cached_network_image.dart';
+import 'package:fwfh_url_launcher/fwfh_url_launcher.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../buildcontext_extension.dart';
+import '../../db/models/article.drift.dart';
+import '../../providers/article.dart';
+import '../../providers/reading_settings.dart';
+
+class HtmlWidgetFactory extends WidgetFactory
+    with CachedNetworkImageFactory, UrlLauncherFactory {
+  HtmlWidgetFactory({this.onTreeBuilt});
+
+  void Function(Widget)? onTreeBuilt;
+
+  @override
+  Widget buildBodyWidget(BuildContext context, Widget child) {
+    final body = super.buildBodyWidget(context, child);
+    onTreeBuilt?.call(body);
+    return body;
+  }
+}
+
+class HtmlWidgetPlus extends StatelessWidget {
+  const HtmlWidgetPlus(
+    this.html, {
+    super.key,
+    this.onTreeBuilt,
+    this.textStyle,
+  });
+
+  final String html;
+  final void Function(Widget)? onTreeBuilt;
+  final TextStyle? textStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return HtmlWidget(
+      html,
+      factoryBuilder: () => HtmlWidgetFactory(
+        onTreeBuilt: (child) =>
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+          onTreeBuilt?.call(child);
+        }),
+      ),
+      textStyle: textStyle,
+    );
+  }
+}
+
+class ArticleContent extends ConsumerStatefulWidget {
+  const ArticleContent({
+    super.key,
+    required this.article,
+    required this.scrollKey,
+  });
+
+  final Article article;
+  final GlobalKey<NestedScrollViewState> scrollKey;
+
+  @override
+  ConsumerState<ArticleContent> createState() => _ArticleContentState();
+}
+
+class _ArticleContentState extends ConsumerState<ArticleContent> {
+  ScrollController get controller =>
+      widget.scrollKey.currentState!.innerController;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.removeListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    final pixels = controller.position.pixels;
+    final maxExtent = controller.position.maxScrollExtent;
+    final double progress = (pixels / maxExtent).clamp(0, 1);
+    ref.read(currentArticleProvider.notifier).saveScrollProgress(progress);
+  }
+
+  // As of version 2.0 there seem to be a random issue with the correct scroll
+  // position being restored. The offset is kind of correct so it should be ok
+  // on a normal usage but it's not perfect.
+  Future<void> _jumpToProgress() async {
+    final scrollPosition =
+        await ref.read(scrollPositionProvider(widget.article.id).future);
+    final progress = scrollPosition?.progress;
+    if (progress != null && progress > 0) {
+      final pixels = progress * controller.position.maxScrollExtent;
+      controller.jumpTo(pixels);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // TODO show a progress indicator to compensate the lack of scrollbar
+        // LinearProgressIndicator(),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                PaddedGroup(
+                  child: HtmlWidgetPlus(
+                    widget.article.content!,
+                    onTreeBuilt: (_) => _jumpToProgress(),
+                    textStyle: ref.watch(readingSettingsProvider).textStyle,
+                  ),
+                ),
+                SizedBox(height: MediaQuery.paddingOf(context).bottom),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ArticleContentEmpty extends StatelessWidget {
+  const ArticleContentEmpty({
+    super.key,
+    required this.articleUrl,
+  });
+
+  final Uri articleUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            context.L.article_noContentFetched,
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+          C.spacers.verticalContent,
+          ElevatedButton(
+            onPressed: () => launchUrl(articleUrl),
+            child: Text(context.L.article_openInBrowser),
+          ),
+        ],
+      ),
+    );
+  }
+}
