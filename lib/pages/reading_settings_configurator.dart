1f7c5cb239f6be2f6aa179013a53a918b3644ee1
Martin Chaine <martin@casimir.me>
2024-12-07 14:31:50 +0100

    refactor(reading settings): use cadanse instead of hard coded values

--- lib/pages/reading_settings_configurator.dart
+++ lib/pages/reading_settings_configurator.dart
@@ -1,19 +1,12 @@
+import 'package:cadanse/cadanse.dart';
+import 'package:cadanse/components/layouts/grouping.dart';
+import 'package:cadanse/tokens/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:universal_platform/universal_platform.dart';
 
 import '../buildcontext_extension.dart';
 import '../providers/reading_settings.dart';
-
-const defaultPadding = 10.0;
-const defaultSpacing = 16.0;
-final defaultRunSpacing = !UniversalPlatform.isMobile ? 10.0 : 0.0;
-const leftAlignedInsets = EdgeInsets.only(
-  left: defaultSpacing,
-  top: defaultPadding,
-  right: defaultPadding,
-  bottom: defaultPadding,
-);
 
 class ReadingSettingsConfigurator extends ConsumerWidget {
   const ReadingSettingsConfigurator({super.key});
@@ -22,59 +15,72 @@ class ReadingSettingsConfigurator extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final values = ref.watch(readingSettingsProvider);
 
-    return ListView(
-      shrinkWrap: true,
-      children: [
-        _buildSectionHeader(context, context.L.readingsettings_fontSize),
-        Padding(
-          padding: const EdgeInsets.all(defaultPadding),
-          child: Slider(
-            value: values.fontSize,
-            min: 12.0,
-            max: 20.0,
-            divisions: 4,
-            onChanged: (size) {
-              ref.read(readingSettingsProvider.notifier).fontSize = size;
-            },
-          ),
-        ),
-        _buildSectionHeader(context, context.L.readingsettings_fontFamily),
-        Padding(
-          padding: const EdgeInsets.all(defaultPadding),
-          child: Wrap(
-            alignment: WrapAlignment.center,
-            spacing: defaultSpacing,
-            runSpacing: defaultRunSpacing,
-            children: readingFonts.map((family) {
-              return ChoiceChip(
-                label: Text(
-                  family,
-                  style: textStyleFromFontFamily(family),
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: PaddedGroup(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ..._buildSection(
+                context,
+                context.L.readingsettings_fontSize,
+                Slider(
+                  value: values.fontSize,
+                  min: 12.0,
+                  max: 20.0,
+                  divisions: 4,
+                  onChanged: (size) {
+                    ref.read(readingSettingsProvider.notifier).fontSize = size;
+                  },
                 ),
-                selected: values.fontFamily == family,
-                onSelected: (selected) {
-                  if (selected) {
-                    ref.read(readingSettingsProvider.notifier).fontFamily =
-                        family;
-                  }
-                },
-              );
-            }).toList(),
+              ),
+              C.spacers.verticalContent,
+              ..._buildSection(
+                context,
+                context.L.readingsettings_fontFamily,
+                Wrap(
+                  alignment: WrapAlignment.center,
+                  spacing: kSpacingInGroup,
+                  runSpacing: !UniversalPlatform.isMobile ? 10.0 : 0.0,
+                  children: readingFonts.map((family) {
+                    return ChoiceChip(
+                      label: Text(
+                        family,
+                        style: textStyleFromFontFamily(family),
+                      ),
+                      selected: values.fontFamily == family,
+                      onSelected: (selected) {
+                        if (selected) {
+                          ref
+                              .read(readingSettingsProvider.notifier)
+                              .fontFamily = family;
+                        }
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
 
-Widget _buildSectionHeader(BuildContext context, String label) {
-  final filterLabelStyle = Theme.of(context)
-      .textTheme
-      .bodyLarge!
-      .copyWith(fontWeight: FontWeight.bold);
-
-  return Padding(
-    padding: leftAlignedInsets,
-    child: Text(label, style: filterLabelStyle),
-  );
+List<Widget> _buildSection(BuildContext context, String label, Widget child) {
+  return [
+    Text(
+      label,
+      style: Theme.of(context)
+          .textTheme
+          .titleMedium
+          ?.copyWith(fontWeight: FontWeight.bold),
+    ),
+    C.spacers.verticalComponent,
+    SizedBox(
+      width: double.infinity,
+      child: child,
+    ),
+  ];
 }
