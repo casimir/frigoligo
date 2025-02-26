c3f3002da0c71a4777d70f659c0d52f518a1064f
Martin Chaine <martin@casimir.me>
2025-02-26 17:28:24 +0100

    fix(ci): update xcode cloud builds (#335)
    * fix(ci): fixes for the iOS pipeline (pin flutter + error handling)
    
    * duplicate the fixes for macos
    
    * remove test project
    
    * let's just build the app
    
    * revert pre-build line

--- macos/ci_scripts/ci_post_clone.sh
+++ macos/ci_scripts/ci_post_clone.sh
@@ -1,23 +1,18 @@
-#!/bin/sh
+#!/bin/sh -xe
 
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
-# Install Flutter using git.
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+FLUTTER_VERSION=$(awk '/"flutter":/{print $2}' .fvmrc | tr -d '"')
+git clone https://github.com/flutter/flutter.git --depth 1 -b $FLUTTER_VERSION $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
 
-# Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
+flutter config --no-analytics --no-cli-animations
+flutter --version
 flutter precache --macos
-
-# Install Flutter dependencies.
 flutter pub get
 
-# Install CocoaPods using Homebrew.
-HOMEBREW_NO_AUTO_UPDATE=1 # disable homebrew's automatic updates.
+HOMEBREW_NO_AUTO_UPDATE=1 
 brew install cocoapods
 
-# Build the macOS app. It takes care of running `pod install`.
 flutter build macos
-
-exit 0
