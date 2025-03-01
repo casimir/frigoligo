d6282dc00f94da60f6c5c41a195c9c3ad60bf431
Martin Chaine <martin@casimir.me>
2025-03-01 17:47:07 +0100

    move the article title to the body & the reading progress bar to the top

--- lib/widgets/html_widget_plus.dart
+++ lib/widgets/html_widget_plus.dart
@@ -21,12 +21,14 @@ class HtmlWidgetPlus extends StatelessWidget {
   const HtmlWidgetPlus(
     this.html, {
     super.key,
+    this.title,
     this.onTreeBuilt,
     this.justifyText,
     this.textStyle,
   });
 
   final String html;
+  final String? title;
   final void Function(Widget)? onTreeBuilt;
   final bool? justifyText;
   final TextStyle? textStyle;
@@ -38,11 +40,13 @@ class HtmlWidgetPlus extends StatelessWidget {
     // 1 em ~ 2 characters (1 em is the width of 'M')
     style += 'max-width:40em;';
 
+    final prefix = title != null ? '<h1>$title</h1>' : '';
+
     return Container(
       alignment: Alignment.center,
       width: double.infinity,
       child: HtmlWidget(
-        '<div style="$style">$html</div>',
+        '$prefix<div style="$style">$html</div>',
         factoryBuilder: () => HtmlWidgetFactory(
           onTreeBuilt: (child) =>
               WidgetsBinding.instance.addPostFrameCallback((_) {
