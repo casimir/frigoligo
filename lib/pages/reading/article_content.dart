72053d0169f84744e53efebc7e57c2c4ac91debb
Martin Chaine <martin@casimir.me>
2025-03-14 12:04:51 +0100

    simplify scrolling tracking

--- lib/pages/reading/article_content.dart
+++ lib/pages/reading/article_content.dart
@@ -21,48 +21,16 @@ class ArticleContent extends ConsumerStatefulWidget {
 }
 
 class _ArticleContentState extends ConsumerState<ArticleContent> {
-  double? _lastSavedProgress;
-  ScrollController? _scroller;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _scroller = PrimaryScrollController.of(context);
-      _scroller!.addListener(_scrollListener);
-    });
-  }
-
-  @override
-  void dispose() {
-    _scroller?.removeListener(_scrollListener);
-    super.dispose();
-  }
-
-  Future<void> _scrollListener() async {
-    final controller = _scroller!;
-    final pixels = controller.position.pixels;
-    final maxExtent = controller.position.maxScrollExtent;
-    final double progress = (pixels / maxExtent).clamp(0, 1);
-    await _onScrollUpdate(progress);
-  }
-
-  Future<void> _onScrollUpdate(double progress) async {
+  Future<void> _onScroll(double progress, bool isScrolling) async {
     ref.read(currentReadingProgressProvider.notifier).progress = progress;
 
-    // throttle saving in DB by requiring at least some progress (in %)
-    const percThreshold = 1 / 100;
-    final delta = _lastSavedProgress != null
-        ? (_lastSavedProgress! - progress).abs()
-        : percThreshold;
-    if (delta >= percThreshold) {
+    if (!isScrolling) {
       await DB().articlesDao.saveScrollProgress(widget.article, progress);
-      _lastSavedProgress = progress;
     }
   }
 
-  Future<void> _jumpToProgress() async {
-    final controller = _scroller!;
+  Future<void> _jumpToProgress(BuildContext context) async {
+    final controller = PrimaryScrollController.of(context);
     final scrollPosition =
         await ref.read(scrollPositionProvider(widget.article.id).future);
     final progress = scrollPosition?.progress;
@@ -76,21 +44,43 @@ class _ArticleContentState extends ConsumerState<ArticleContent> {
   Widget build(BuildContext context) {
     final settings = ref.watch(readingSettingsProvider);
     return SelectionArea(
-      child: SingleChildScrollView(
-        primary: true,
-        child: Column(
-          children: [
-            PaddedGroup(
-              child: HtmlWidgetPlus(
-                widget.article.content!,
-                title: widget.article.title,
-                onTreeBuilt: (_) => _jumpToProgress(),
-                justifyText: settings.justifyText,
-                textStyle: settings.textStyle,
+      child: NotificationListener<ScrollNotification>(
+        onNotification: (notification) {
+          var update = false;
+          var isScrolling = false;
+          if (notification is ScrollUpdateNotification) {
+            update = true;
+            isScrolling = true;
+          }
+          if (notification is ScrollEndNotification) {
+            update = true;
+          }
+
+          if (update) {
+            final pixels = notification.metrics.pixels;
+            final maxExtent = notification.metrics.maxScrollExtent;
+            final double progress = (pixels / maxExtent).clamp(0, 1);
+            _onScroll(progress, isScrolling);
+          }
+
+          return false;
+        },
+        child: SingleChildScrollView(
+          primary: true,
+          child: Column(
+            children: [
+              PaddedGroup(
+                child: HtmlWidgetPlus(
+                  widget.article.content!,
+                  title: widget.article.title,
+                  onTreeBuilt: (_) => _jumpToProgress(context),
+                  justifyText: settings.justifyText,
+                  textStyle: settings.textStyle,
+                ),
               ),
-            ),
-            SizedBox(height: MediaQuery.paddingOf(context).bottom),
-          ],
+              SizedBox(height: MediaQuery.paddingOf(context).bottom),
+            ],
+          ),
         ),
       ),
     );
