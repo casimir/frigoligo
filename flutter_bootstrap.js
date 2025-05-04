f43e8a71f08495274fe1582ad229d8372a9944a7
casimir <casimir@users.noreply.github.com>
2025-05-04 08:54:21 +0000

    deploy: 63ec82f432806b594bb53cb32ff20580edb3e03e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3386660930"
+    serviceWorkerVersion: "1149676067"
   }
 });
