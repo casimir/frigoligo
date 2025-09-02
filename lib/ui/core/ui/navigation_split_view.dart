285f926d3572c48ebcce61fecebf19c8df587077
Martin Chaine <martin@casimir.me>
2025-09-02 16:06:22 +0200

    all done!

--- lib/ui/core/ui/navigation_split_view.dart
+++ lib/ui/core/ui/navigation_split_view.dart
@@ -2,11 +2,3 @@ export 'src/navigation_split_view/expand_content_button.dart'
     show ExpandContentButton;
 export 'src/navigation_split_view/navigation_split_view.dart'
     show NavigationSplitView, NavigationSplitViewState;
-
-// TODO for unit tests
-// 1. ✅ correct layout (check for different window classes and resize)
-// 2. ✅ correct update on item selection (and pre-selection)
-// 3. ✅ toggle (programmatically and with expander toggle)
-// 4. ✅ animation on toggle (doable?)
-// 5. ✅ correct behavior on shortcuts triggered
-// 6. jumpTo initialIndex so it is visible if necessary
