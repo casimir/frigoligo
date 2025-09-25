42e01be462d1a932f2d6f863402f54d669666410
Martin Chaine <martin@casimir.me>
2025-09-25 14:02:32 +0200

    test LogConsoleViewModel

---
+++ test/ui/logconsole/viewmodels/logconsole_viewmodel_test.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:frigoligo/data/repositories/logger_repository.dart';
+import 'package:frigoligo/data/services/platform/sharing/sharing_service.dart';
+import 'package:frigoligo/domain/repositories.dart';
+import 'package:frigoligo/ui/logconsole/viewmodels/logconsole_viewmodel.dart';
+import 'package:logging/logging.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../../testing/services/inmemory_logging_storage_service.dart';
+
+class MockSharingService extends Mock implements SharingService {}
+
+void main() {
+  group('LogConsoleViewModel', () {
+    late LogConsoleViewModel viewModel;
+    late LoggerRepository loggerRepository;
+    late MockSharingService mockSharingService;
+
+    setUp(() {
+      loggerRepository = LoggerRepositoryImpl(
+        loggingStorageService: InMemoryLoggingStorageService(),
+        startingAppMessage: 'BEGIN',
+        maxLogCount: 10,
+      )..registerLogHandler(false);
+
+      Logger.root.info('pre begin');
+      Logger.root.info('BEGIN');
+      Logger.root.info('post begin');
+
+      mockSharingService = MockSharingService();
+      viewModel = LogConsoleViewModel(
+        loggerRepository: loggerRepository,
+        sharingService: mockSharingService,
+      );
+    });
+
+    tearDown(() {
+      Logger.root.clearListeners();
+    });
+
+    test('get*LogCount methods should return the correct counts', () async {
+      expect(await viewModel.getLogCount(), equals(3));
+      expect(await viewModel.getCurrentRunLogCount(), equals(2));
+    });
+
+    test('clearLogs() should clear logs and notify listeners', () async {
+      int notifyListenersCount = 0;
+      viewModel.addListener(() {
+        notifyListenersCount++;
+      });
+
+      await viewModel.clearLogs();
+
+      expect(await viewModel.getLogCount(), equals(0));
+      expect(notifyListenersCount, equals(1));
+    });
+
+    group('shareLogs(...)', () {
+      test(
+        'should share current run logs when onlyCurrentRun is true',
+        () async {
+          when(
+            () => mockSharingService.shareAsTextFile(
+              any(),
+              any(),
+              sharePositionOrigin: any(named: 'sharePositionOrigin'),
+            ),
+          ).thenAnswer((_) async {});
+
+          await viewModel.shareLogs(true, Rect.zero);
+
+          final captured =
+              verify(
+                () => mockSharingService.shareAsTextFile(
+                  captureAny(),
+                  captureAny(),
+                  sharePositionOrigin: any(named: 'sharePositionOrigin'),
+                ),
+              ).captured;
+
+          final content = captured[1] as String;
+          expect(content, equals('BEGIN\npost begin'));
+        },
+      );
+
+      test('should share all logs when onlyCurrentRun is false', () async {
+        when(
+          () => mockSharingService.shareAsTextFile(
+            any(),
+            any(),
+            sharePositionOrigin: any(named: 'sharePositionOrigin'),
+          ),
+        ).thenAnswer((_) async {});
+
+        await viewModel.shareLogs(false, Rect.zero);
+
+        final captured =
+            verify(
+              () => mockSharingService.shareAsTextFile(
+                captureAny(),
+                captureAny(),
+                sharePositionOrigin: any(named: 'sharePositionOrigin'),
+              ),
+            ).captured;
+
+        final content = captured[1] as String;
+        expect(content, equals('pre begin\nBEGIN\npost begin'));
+      });
+    });
+  });
+}
