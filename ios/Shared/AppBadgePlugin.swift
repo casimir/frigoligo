ea3052038d0a207ccf99e55755e9537cf5a8a3d0
Martin Chaine <martin@casimir.me>
2026-01-15 22:38:50 +0100

    add appbadge update support in ios native extension

--- ios/Shared/AppBadgePlugin.swift
+++ ios/Shared/AppBadgePlugin.swift
@@ -1,6 +1,6 @@
 //
 //  AppBadgePlugin.swift
-//  Runner
+//  Shared
 //
 //  Created by Martin Chaine on 14/08/2024.
 //
@@ -8,18 +8,18 @@
 import Flutter
 import UIKit
 
-public class AppBadgePlugin : NSObject, FlutterPlugin {
+public class AppBadgePlugin: NSObject, FlutterPlugin {
     static let CHANNEL = "net.casimir-lab.frigoligo/appbadge"
-    
+
     public static func register(with registrar: any FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: CHANNEL, binaryMessenger: registrar.messenger())
         registrar.addMethodCallDelegate(AppBadgePlugin(), channel: channel)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         UNUserNotificationCenter.current().requestAuthorization(options: [.badge]) { granted, error in
         }
-        
+
         switch call.method {
         case "update":
             let arguments = call.arguments as? Dictionary<String, Any>
@@ -33,12 +33,14 @@ public class AppBadgePlugin : NSObject, FlutterPlugin {
             result(FlutterMethodNotImplemented)
         }
     }
-    
+
     private func setBadgeCount(_ count: Int) {
         if #available(iOS 16.0, *) {
             UNUserNotificationCenter.current().setBadgeCount(count)
-        } else {
-            UIApplication.shared.applicationIconBadgeNumber = count
+        } else if !Bundle.main.bundlePath.hasSuffix(".appex"),
+                  let application = UIApplication.value(forKey: "sharedApplication") as? UIApplication {
+            // UIApplication.shared is not available in app extensions
+            application.applicationIconBadgeNumber = count
         }
     }
 }
