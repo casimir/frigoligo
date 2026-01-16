5a0addc653785d7b0c18d049a60276948344faae
casimir <casimir@users.noreply.github.com>
2026-01-16 21:48:11 +0000

    deploy: 80fe0fc42c541c823978e39c87e27d878891d58a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -254,7 +254,7 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "cb39c897b54b010dcf1528ac0291f397",
 "sqlite3.wasm": "9839e2a1f55c56501c36b8e8483ee663",
-"flutter_bootstrap.js": "5bb7117c557baae2cb15ac7983367a5d",
+"flutter_bootstrap.js": "851ff41b3c32bb0020adc2fcf69b1d74",
 "version.json": "0351f2f681def5caba2c2b3553e23b05",
 "main.dart.js": "c93b32a3b2d77262371e35cc90f51cc8",
 "drift_worker.js": "afac8b57eb80f0846a382f7303929b0f"};
